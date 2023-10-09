a0bcd41d7cb5a45bc98cf962db5a10c5e95276dd
champix56 <desorbaix@free.fr>
2023-10-09 14:51:59 +0200

    mise en oeuvre d'un composant sans props

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import Button from './components/uis/Button/Button';
 
 function App() {
   return (
     <div className="App">
       DEMAT BREIZH
+      <hr/>
+      <Button/>
+      <Button/>
+      <Button/>
+      <Button/>
     </div>
   );
 }
