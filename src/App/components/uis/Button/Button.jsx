1e640b842dc413dc2cc5e1764c82b666c73116e3
champix56 <desorbaix@free.fr>
2023-10-09 15:23:13 +0200

    ajout du css simple

--- src/App/components/uis/Button/Button.jsx
+++ src/App/components/uis/Button/Button.jsx
@@ -1,15 +1,19 @@
 import React from "react";
+import './Button.css'
 
 /*function Button(props) {
   return <button>Benjamin</button>;
 }*/
+
 const Button = (props) => {
   console.log(props);
   return (
     <button
       className="Button"
       onClick={(evt) => {
+        //traitement de l'event prealable propre au composant
         console.log(evt);
+        //declenchement de la fonction envoyée par les props depuis le parent
         props.onbuttonclick("on a tapper sur benjamin");
       }}
     >
