56803fd7c369deb2cae4835a2ba5a57d682f93f5
champix56 <desorbaix@free.fr>
2023-10-09 15:01:16 +0200

    transfert differents types dans les props

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,16 +1,27 @@
-import React from 'react';
-import './App.css';
-import Button from './components/uis/Button/Button';
+import React from "react";
+import "./App.css";
+import Button from "./components/uis/Button/Button";
 
 function App() {
   return (
     <div className="App">
       DEMAT BREIZH
-      <hr/>
-      <Button text="Valider"/>
-      <Button text="Annuler"/>
-      <Button text="Benjamin"/>
-      <Button/>
+      <hr />
+      <Button text="Valider" />
+      <Button text="Annuler" />
+      <Button text="Benjamin" />
+      <Button
+        x={[1, 2]}
+        c={{ abc: 123 }}
+        s="cc"
+        n={1}
+        b={true}
+        g={undefined}
+        f={() => {
+          console.log("fn");
+        }}
+        nn={null}
+      />
     </div>
   );
 }
