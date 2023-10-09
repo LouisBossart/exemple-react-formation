7bfe84ad3544b4eb4c746ef24651257f02fe0286
champix56 <desorbaix@free.fr>
2023-10-09 14:50:16 +0200

    composant minimum

---
+++ src/App/components/uis/Button/Button.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+/*function Button(props) {
+  return <button>Benjamin</button>;
+}*/
+const Button = (props) => {
+    return <button className="Button">Bejamin</button>
+}
+
+export default Button;
