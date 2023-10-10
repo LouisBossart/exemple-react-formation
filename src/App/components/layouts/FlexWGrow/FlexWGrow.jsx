2e2c0928391c05e61f9d10eaa0d2caea8c5e15d9
champix56 <desorbaix@free.fr>
2023-10-10 12:30:11 +0200

    generation des composants

---
+++ src/App/components/layouts/FlexWGrow/FlexWGrow.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./FlexWGrow.module.css";
+
+const FlexWGrow = (props) => {
+  return (
+    <div className={styles.FlexWGrow} data-testid="FlexWGrow">
+      {props.children}
+    </div>
+  );
+};
+
+FlexWGrow.propTypes = {
+  children: PropTypes.any.isRequired,
+};
+
+FlexWGrow.defaultProps = {};
+
+export default FlexWGrow;
