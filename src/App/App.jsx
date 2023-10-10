7baccb78f150223a594362e793938268056cfe7d
champix56 <desorbaix@free.fr>
2023-10-10 17:45:33 +0200

    gestion loading async fetch

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import FlexHGrow from "./components/layouts/FlexHGrow/FlexHGrow";
 import Header from "./components/uis/Header/Header";
@@ -8,19 +8,29 @@ import MemeForm from "./components/functionnals/MemeForm/MemeForm";
 import Footer from "./components/uis/Footer/Footer";
 import { MemeSVGViewer, emptyMeme } from "orsys-tjs-meme";
 
-
 function App(props) {
-  const [state, setstate] = useState(emptyMeme)
+  const [state, setstate] = useState(emptyMeme);
+  const [images, setimages] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5679/images")
+      .then((resp) => resp.json())
+      .then((arr) => setimages(arr));
+  }, []);
   return (
     <div className="App">
       <FlexHGrow>
         <Header />
         <Navbar />
         <FlexWGrow>
-          <MemeSVGViewer basePath="" meme={state}/>
-          <MemeForm meme={state} onMemeChange={(meme)=>{
-            setstate(meme);
-          }} />
+          <MemeSVGViewer basePath="" meme={state} />
+          <MemeForm
+            images={images}
+            meme={state}
+            onMemeChange={(meme) => {
+              setstate(meme);
+            }}
+          />
         </FlexWGrow>
         <Footer />
       </FlexHGrow>
