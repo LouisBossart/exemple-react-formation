9816f54ca76fc147fa0358109c88db7777cf7b93
champix56 <desorbaix@free.fr>
2023-10-10 16:32:55 +0200

    2way binding d'etat wt diffusion plusieurs enfants

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,22 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import FlexHGrow from "./components/layouts/FlexHGrow/FlexHGrow";
 import Header from "./components/uis/Header/Header";
 import Navbar from "./components/uis/Navbar/Navbar";
 import FlexWGrow from "./components/layouts/FlexWGrow/FlexWGrow";
-import MemeSvgViewer from "./components/uis/MemeSVGViewer/MemeSVGViewer";
 import MemeForm from "./components/functionnals/MemeForm/MemeForm";
 import Footer from "./components/uis/Footer/Footer";
+import { MemeSVGViewer, emptyMeme } from "orsys-tjs-meme";
+
 
 function App(props) {
+  const [state, setstate] = useState(emptyMeme)
   return (
     <div className="App">
       <FlexHGrow>
         <Header />
         <Navbar />
         <FlexWGrow>
-          <MemeSvgViewer />
-          <MemeForm />
+          <MemeSVGViewer basePath="" meme={state}/>
+          <MemeForm meme={state} onMemeChange={(meme)=>{
+            setstate(meme);
+          }} />
         </FlexWGrow>
         <Footer />
       </FlexHGrow>
