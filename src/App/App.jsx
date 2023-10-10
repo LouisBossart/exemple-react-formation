a688ef73ceef63b55f6cbc3023afde1c2f15a921
champix56 <desorbaix@free.fr>
2023-10-10 12:48:23 +0200

    construction de base de notre app

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,47 +1,25 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./App.css";
-import Button from "./components/uis/Button/Button";
+import FlexHGrow from "./components/layouts/FlexHGrow/FlexHGrow";
+import Header from "./components/uis/Header/Header";
+import Navbar from "./components/uis/Navbar/Navbar";
+import FlexWGrow from "./components/layouts/FlexWGrow/FlexWGrow";
+import MemeSvgViewer from "./components/uis/MemeSVGViewer/MemeSVGViewer";
+import MemeForm from "./components/functionnals/MemeForm/MemeForm";
+import Footer from "./components/uis/Footer/Footer";
 
 function App(props) {
-  const [counter, setcounter] = useState(-1);
-  //Mounté uniquement
-  useEffect(() => {
-    //corps du cycle de vie
-    console.log("montage du composant")
-    setcounter(0);
-    //fonction de demontage de composant
-    // return () => {
-    //   //effect
-    //   //
-    // };
-  },[])
-  //mounter et mis a jour
-  useEffect(()=>{
-    console.log('didUpdate sur effect',counter);
-  },[counter])
   return (
     <div className="App">
-      Valeur counter : {counter}<br/>
-      {/* Valeur counter de userState: {userState.value}<br/> */}
-      <hr />
-      <Button
-        onbuttonclick={() => {
-          setcounter(counter-1);
-          //setUserState({...userState,value:userState.value-1})
-          console.log(counter);
-        }}
-      >
-        -1
-      </Button>
-      <Button
-        text="Annuler"
-        onbuttonclick={() => {
-          setcounter(counter+1);
-          console.log(counter);
-        }}
-      >
-        +1
-      </Button>
+      <FlexHGrow>
+        <Header />
+        <Navbar />
+        <FlexWGrow>
+          <MemeSvgViewer />
+          <MemeForm />
+        </FlexWGrow>
+        <Footer />
+      </FlexHGrow>
     </div>
   );
 }
