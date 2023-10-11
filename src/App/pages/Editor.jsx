db504876d8a5fa1ac809e8200fe2d0a50a276f4e
champix56 <desorbaix@free.fr>
2023-10-11 16:41:39 +0200

    fix import de navbar

--- src/App/pages/Editor.jsx
+++ src/App/pages/Editor.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import FlexHGrow from '../components/layouts/FlexHGrow/FlexHGrow';
 import Header from '../components/uis/Header/Header';
-import { Navbar } from 'react-bootstrap';
+import Navbar  from '../components/uis/Navbar/Navbar';
 import FlexWGrow from '../components/layouts/FlexWGrow/FlexWGrow';
 import MemeSvgViewer from '../components/uis/MemeSVGViewer/MemeSVGViewer';
 import { ConnectedMemeForm } from '../components/functionnals/MemeForm/MemeForm';
