84229dfea9fa80e449a299492efb885547ca9a55
champix56 <desorbaix@free.fr>
2023-10-11 16:37:47 +0200

    deplacement des composants pages

---
+++ src/App/pages/Editor.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import FlexHGrow from '../components/layouts/FlexHGrow/FlexHGrow';
+import Header from '../components/uis/Header/Header';
+import { Navbar } from 'react-bootstrap';
+import FlexWGrow from '../components/layouts/FlexWGrow/FlexWGrow';
+import MemeSvgViewer from '../components/uis/MemeSVGViewer/MemeSVGViewer';
+import { ConnectedMemeForm } from '../components/functionnals/MemeForm/MemeForm';
+import Footer from '../components/uis/Footer/Footer';
+
+export default function Editor() {
+  return (
+    <>
+     <FlexHGrow>
+        <Header />
+        <Navbar />
+        <FlexWGrow>
+          <MemeSvgViewer basePath="" />
+          <ConnectedMemeForm />
+        </FlexWGrow>
+        <Footer />
+      </FlexHGrow>
+    </>
+  );
+}
