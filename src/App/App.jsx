7fd60ed788c176b0c1548f9e6efdc128b3c76754
champix56 <desorbaix@free.fr>
2023-10-11 16:38:04 +0200

    ajout de liens et de routes

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -10,29 +10,29 @@ import MemeForm, {
 import Footer from "./components/uis/Footer/Footer";
 import { emptyMeme } from "orsys-tjs-meme";
 import { store } from "./store/store";
-import MemeSVGViewer from './components/uis/MemeSVGViewer/MemeSVGViewer'
-import {Modal} from "./components/Modal/Modal";
-import {useDispatch} from 'react-redux'
-//import { fillImages } from "./store/ressourcesSlice";
+import MemeSVGViewer from "./components/uis/MemeSVGViewer/MemeSVGViewer";
+import { Modal } from "./components/Modal/Modal";
+import { Link, Route, Routes } from "react-router-dom";
+import Thumbnail from "./pages/thumbnail";
+import Editor from "./pages/Editor";
 function App(props) {
-  //const d=useDispatch()
-//  useEffect(() => {
-//    fetch('http://localhost:5679/images').then(r=>r.json()).then(arr=>d(fillImages({images:arr})))
-//  }, [])
   return (
     <>
-    <div className="App">
-      <FlexHGrow>
-        <Header />
-        <Navbar />
-        <FlexWGrow>
-          <MemeSVGViewer basePath="" />
-          <ConnectedMemeForm />
-        </FlexWGrow>
-        <Footer />
-      </FlexHGrow>
-    </div>
-    <Modal/>
+      <div className="App">
+        <div>
+          <Link to="/">HOME</Link>
+          <Link to="/thumbnail"> |Thumbnail</Link>
+          <Link to="/editor"> | EDITOR</Link>
+          <Link to="/editor/2"> | EDITOR/2</Link>
+        </div>
+        <Routes>
+          <Route path="/" element={<div>Bienvenue</div>}></Route>
+          <Route path="/thumbnail" element={<Thumbnail />} />
+          <Route path="/editor" element={<Editor />} />
+          <Route path="/editor/:id" element={<Editor/>} />
+        </Routes>
+      </div>
+      <Modal />
     </>
   );
 }
