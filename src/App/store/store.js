89c479d36db7b0df9c3c4f730ef80bffb4082830
champix56 <desorbaix@free.fr>
2023-10-11 10:37:37 +0200

    ajout d'une tranche de reducer + store comniner

---
+++ src/App/store/store.js
@@ -0,0 +1,18 @@
+import {combineReducers, configureStore} from '@reduxjs/toolkit'
+import currentReducer from './currentSlice'
+import modalReducer from './modalSlice'
+export const store=configureStore({
+    reducer:combineReducers({current: currentReducer, modal:modalReducer})
+});
+
+
+// console.log(changeMeme({text:'coucou'}));
+
+// console.log(store.getState());
+
+// store.subscribe(()=>{
+//     console.log(store.getState(),store.getState().current.titre);
+// })
+// store.dispatch({type:'current/changeMeme',payload:{titre:'coucou'}});
+// store.dispatch(changeMeme({text:'mon nouveau meme'}));
+// store.dispatch(clearMeme());
