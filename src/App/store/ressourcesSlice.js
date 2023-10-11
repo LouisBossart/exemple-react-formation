5263c8c1a8e700374605bd021fb3c2bd7c88b348
champix56 <desorbaix@free.fr>
2023-10-11 14:45:06 +0200

    constantes + loadDatas

--- src/App/store/ressourcesSlice.js
+++ src/App/store/ressourcesSlice.js
@@ -2,28 +2,27 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { RESSOURCES_URL, REST_ADR } from "../config/constante";
 
 const initialState = { images: [], memes: [] };
-// function isActionWithNumberPayload(action) {
-//   const r=/ressources\/loadImages\/*/;
+function isActionWithNumberPayload(action) {
+  const r=/ressources\/load\w*\/rejected/;
 
-//   const m = r.exec(action.type);
-//     return m!==null;
-// }
+  const m = r.exec(action.type);
+    return m!==null;
+}
 const ressourcesSlice = createSlice({
   name: "ressources",
   initialState,
-  reducers: {
-    // fillImages:(state,action)=>{
-    //     state.images.splice(0);
-    //     state.images.push(...action.payload.images)
-    // }
-  },
+  reducers: {},
   extraReducers: (builder) => {
     builder.addCase('ressources/loadImages/fulfilled',(state,action)=>{
         state.images.splice(0);
         state.images.push(...action.payload);
     });
-    builder.addCase('ressources/loadImages/rejected',()=>{console.log('erreur de chargement')})
-    // builder.addMatcher(isActionWithNumberPayload, (s, a) => console.log(a));
+    builder.addCase('ressources/loadmemes/fulfilled',(state,action)=>{
+        state.memes.splice(0);
+        state.memes.push(...action.payload);
+    });
+    // builder.addCase('ressources/loadImages/rejected',()=>{console.log('erreur de chargement')})
+    builder.addMatcher(isActionWithNumberPayload, (s, a) => console.error('ERREUR DE CHARGEMENT une requete est rejetée',a));
   },
 });
 export const loadImages = createAsyncThunk(
@@ -33,6 +32,13 @@ export const loadImages = createAsyncThunk(
     return await primage.json();
   }
 );
+export const loadMemes = createAsyncThunk(
+    "ressources/loadMemes",
+    async () => {
+      const primage = await fetch(`${REST_ADR}${RESSOURCES_URL.memes}`);
+      return await primage.json();
+    }
+  );
 //export const {fillImages} = ressourcesSlice.actions
 
 export default ressourcesSlice.reducer;
